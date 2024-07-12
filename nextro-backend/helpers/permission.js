52fd28eb6ed45a3403eddcc6e73d5030cc2f3a22
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-12 11:20:03 +0200

    User permissions 🔐: for admin, general user or user not found scenarios >>> uploadProductPermission

---
+++ nextro-backend/helpers/permission.js
@@ -0,0 +1,17 @@
+/**
+ * User permissions
+ */
+const userModel = require("../models/userModel")
+
+const uploadProductPermission = async(userId) => {
+    const user = await userModel.findById(userId)
+
+    if(user.role === 'ADMIN'){
+        return true
+    }
+
+    return false
+}
+
+
+module.exports = uploadProductPermission
