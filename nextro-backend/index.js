18c9d5389660f334e7f6630b2b2d17239f5d2f19
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-12 15:34:30 +0200

    🚀 routes update: CORS addition

--- nextro-backend/index.js
+++ nextro-backend/index.js
@@ -5,12 +5,19 @@ const express = require('express');
 require('dotenv'.config);
 const connectDB = require('.config/db');
 const router = require('.routes');
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
 
 /**
  * Express App
  */
 const app = express();
-app.use(express.json());
+app.use(cors({
+    origin : process.env.FRONTEND_URL,
+    credentials : true
+}))
+app.use(express.json())
+app.use(cookieParser())
 
 /**
  * App Router
@@ -21,6 +28,7 @@ app.use("/api", router);
  * Port
  */
 const PORT = 8080 || process.env.PORT
+
 
 connectDB().then(()=>{
     app.listen(PORT,()=>{
