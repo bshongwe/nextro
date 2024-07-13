66419f1908f3b2972c887648946d51a4fb394487
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-13 11:29:13 +0200

    🔐Swagger update for server

--- nextro-backend/index.js
+++ nextro-backend/index.js
@@ -5,29 +5,31 @@ const express = require('express');
 require('dotenv'.config);
 const connectDB = require('.config/db');
 const router = require('.routes');
-const cors = require('cors')
-const cookieParser = require('cookie-parser')
+const cors = require('cors');
+const cookieParser = require('cookie-parser');
 
-/**
- * Express App
- */
+// Swagger Defs
+const swaggerUi = require('swagger-ui-express');
+const swaggerSpec = require('./swagger');
+
+// Express App
 const app = express();
 app.use(cors({
     origin : process.env.FRONTEND_URL,
     credentials : true
-}))
-app.use(express.json())
-app.use(cookieParser())
+}));
+app.use(express.json());
+app.use(cookieParser());
 
-/**
- * App Router
- */
+// App Router
 app.use("/api", router);
 
-/**
- * Port
- */
-const PORT = 8080 || process.env.PORT
+// Swagger
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+
+
+// Port
+const PORT = 8080 || process.env.PORT;
 
 
 connectDB().then(()=>{
@@ -35,4 +37,4 @@ connectDB().then(()=>{
         console.log("connnect to DB")
         console.log("Server is running "+PORT)
     })
-})
+});
