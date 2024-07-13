77cfccbd82b1ce0868070e9c91e0a3e43e029ba7
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-13 11:16:59 +0200

    🔐Swagger docs update

--- nextro-backend/swagger.js
+++ nextro-backend/swagger.js
@@ -9,8 +9,28 @@ const options = {
     info: {
       title: 'NeXtro API',
       version: '1.0.0',
-      description: 'API documentation for the NeXtro eCommerce platform',
+      description: 'SWagger API documentation for the NeXtro eCommerce platform',
+      termsOfService: '<http://example.com/terms/>',
+      contact: {
+        name: 'Ernest Bhekizwe Shongwe',
+        url: 'http://www.github.com/bshongwe',
+        email: 'shongwe.bhekizwe@gmail.com',
+      },
+      license: {
+        name: 'ISC',
+        url: 'http://example.com/license',
+      },
     },
+    servers: [
+      {
+        url: 'http://localhost:8080',
+        description: 'Development server',
+      },
+      {
+        url: 'https://api.nextro.com', // will adjust this
+        description: 'Production server',
+      },
+    ],
   },
   apis: ['./routes/*.js'],
 };
