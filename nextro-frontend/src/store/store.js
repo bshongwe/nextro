58be9f3242d53c8229c911ed2ee75505a3adfb12
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-14 05:23:58 +0200

    🛒🔐 Store config for clientside, uses Redux toolkit

---
+++ nextro-frontend/src/store/store.js
@@ -0,0 +1,12 @@
+/**
+ * Uses reducer to configure clientside of store,
+ * reducer uses configs from redux
+ */
+import { configureStore } from '@reduxjs/toolkit'
+import userReducer from './userSlice'
+
+export const store = configureStore({
+  reducer: {
+    user : userReducer
+  },
+})
