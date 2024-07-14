c5244716553648032d92c049202bfb024e71319a
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-14 20:17:21 +0200

    Fixing compilation errors

--- nextro-frontend/src/index.js
+++ nextro-frontend/src/index.js
@@ -5,6 +5,8 @@ import { Provider  } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import router from './routes';
+import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
