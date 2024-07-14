7e4891b417c2a0cd2244c2fc0721071019441069
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-14 21:21:55 +0200

    Bug fix 5: update routes and libs

--- nextro-frontend/src/index.js
+++ nextro-frontend/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { RouterProvider } from 'react-router-dom';
-import { Provider  } from 'react-redux';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { RouterProvider } from 'react-router-dom';
 import router from './routes';
+import { Provider  } from 'react-redux';
 import { store } from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
