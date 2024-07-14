6fef6f54999d4e2c042cbef6e6f1e48ad2e631ce
bshongwe <dru.da.beat.hustle@gmail.com>
2024-07-14 20:49:54 +0200

    Bug fix for store

--- nextro-frontend/src/store/store.js
+++ nextro-frontend/src/store/store.js
@@ -1,7 +1,5 @@
-/**
- * Uses reducer to configure clientside of store,
- * reducer uses configs from redux
- */
+// Uses reducer to configure clientside of store,
+// reducer uses configs from redux
 import { configureStore } from '@reduxjs/toolkit'
 import userReducer from './userSlice'
 
